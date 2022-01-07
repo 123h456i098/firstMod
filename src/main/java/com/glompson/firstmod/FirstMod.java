3602e0dcf4f7f2c649cdad3840ecc51ae6d1c420
123h456i098 <aletxhompson@gmail.com>
2022-01-07 14:01:43 +1300

    Initial commit

---
+++ src/main/java/com/glompson/firstmod/FirstMod.java
@@ -0,0 +1,33 @@
+package com.glompson.firstmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.system.CallbackI;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(FirstMod.MOD_ID)
+public class FirstMod {
+    public static final String MOD_ID = "firstmod";
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+
+    public FirstMod() {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+        
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {}
+}
